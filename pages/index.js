76f796ebbaea25c9095945dac38b4a229ad4077c
Felipe-Rodrigues01 <129695960+Felipe-Rodrigues01@users.noreply.github.com>
2025-08-16 21:06:21 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Teste2</h1>;
+} // essa function é um componente
+
+export default Home; //essse export é necessário para indicar ao Next que essa função deve ser reenderizada
